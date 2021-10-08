2545588bf65e2f5b6b652fc593475a32151dedbc
Viacheslav Diukov <slavad156@ukr.net>
2021-10-08 18:37:25 +0300

    Почти закончил оформление форм

--- js/form.js
+++ js/form.js
@@ -3,5 +3,16 @@
     e.preventDefault();
 
     new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+
+    e.currentTarget.reset();
+  });
+})();
+
+(() => {
+  document.querySelector('.js-speaker-form-subs').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
+    e.currentTarget.reset();
   });
 })();
